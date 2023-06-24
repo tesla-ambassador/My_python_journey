6f7d972d4585bb5a09f35917b9b6b4e29e8c8ea4
Kevin Emma <Kevin@Genesys.local>
2023-06-24 21:29:22 +0300

    I deleted the NSFW jokes that were in my code

--- day_2_project.py
+++ day_2_project.py
@@ -1,7 +1,7 @@
 print("Welcome to this soulless calculator")
 total_bill = input("What's the total amount you losers spent? $")
-percentage = input("What percentage can your broke a**es afford to give? 10, 12, or 15? ")
-no_people = input("How many wankers are you lot? ")
+percentage = input("What percentage can you broke fellas afford to give? 10, 12, or 15? ")
+no_people = input("How many losers are you lot? ")
 
 tip_percentage = float(total_bill) * (int(percentage)/100)
 tip_per_person = tip_percentage / int(no_people)
@@ -9,6 +9,6 @@ split_bill = float(total_bill)/int(no_people)
 result = round(tip_per_person + split_bill, 2)
 
 if(int(percentage) <= 0):
-    print("This is Uganda, we don't give tips! Work your a**!")
+    print("I'm too poor to afford to tip you lol!")
 else:
     print(f"Each person should pay: ${result}")
