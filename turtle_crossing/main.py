1cd5a98a2001486da5a24562bda345dad734d1d1
Tesla_Ambassador <kevindan641@gmail.com>
2023-09-07 00:39:38 +0300

    Finished Day 23

--- turtle_crossing/main.py
+++ turtle_crossing/main.py
@@ -8,7 +8,28 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+car_manager = CarManager()
+player = Player()
+score = Scoreboard()
+
+screen.listen()
+screen.onkey(player.move_up, 'Up')
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.generate_car()
+    car_manager.move_cars()
+    
+    for car in car_manager.all_cars:
+        if car.distance(player) < 30:
+            score.game_over()
+            game_is_on = False
+            
+    if player.is_reached_finish():
+        score.increment_level()
+        player.reset_pos()
+        car_manager.level_up()
+    
+screen.exitonclick()
--- turtle_crossing/scoreboard.py
+++ turtle_crossing/scoreboard.py
@@ -1,5 +1,26 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.goto(-220, 250)
+        self.level = 1
+        self.update_scoreboard()
+        
+        
+    def update_scoreboard(self):
+        self.write(f"Level: {self.level}", align='center', font=FONT)
+        
+    def increment_level(self):
+        self.clear()
+        self.level += 1
+        self.update_scoreboard()
+        
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", align='center', font=FONT)
